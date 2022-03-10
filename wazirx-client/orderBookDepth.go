ecfb4818ca22551cadb3c94969706a3df4010025
yuvan11 <yuva.ram11@gmail.com>
2022-03-10 16:09:25 +0530

    add orderBook api binding to capture market depth

---
+++ wazirx-client/orderBookDepth.go
@@ -0,0 +1,73 @@
+package wazirx_client
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	helpers "wazirx-go/wazirx-client/Helpers"
+	api "wazirx-go/wazirx-client/endpoints"
+)
+
+type OrderBookDepthService struct {
+	C      *Client
+	Symbol string
+	Limit  int32
+}
+
+/*
+type OrderPrice struct {
+	PriceValue string
+	Quantity   string
+}
+*/
+
+type OrderBookDepth struct {
+	TimeStamp int64      `json:"timestamp"`
+	Asks      [][]string `json:"asks"`
+	Bids      [][]string `json:"bids"`
+}
+
+func (s *OrderBookDepthService) SetSymbol(symbol string) *OrderBookDepthService {
+	s.Symbol = symbol
+	return s
+}
+
+func (s *OrderBookDepthService) SetLimit(limit int32) *OrderBookDepthService {
+	s.Limit = limit
+	return s
+}
+
+func (s *OrderBookDepthService) DisplayOrdersBookDepth(ctx context.Context) *OrderBookDepth {
+
+	r := &Request{
+		Method:     http.MethodGet,
+		Endpoint:   api.MARKET_DEPTH,
+		SecType:    SecTypeSigned,
+		RecvWindow: 10000,
+		TimeStamp:  helpers.CurrentTimestamp(),
+	}
+
+	if s.Limit > 0 {
+		r.SetParam("limit", s.Limit)
+	}
+
+	if s.Symbol != "" {
+		r.SetParam("symbol", s.Symbol)
+	}
+
+	data, err := s.C.HandleAPI(ctx, r)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	//var result map[string]interface{}
+	result := new(OrderBookDepth)
+
+	if err := json.Unmarshal(data, &result); err != nil {
+		fmt.Println("Can not unmarshal JSON")
+	}
+
+	return result
+}
