a9a7b133137e08863cf7b2d5cc972f40fdeb3422
yuvan11 <yuva.ram11@gmail.com>
2022-03-10 16:07:47 +0530

    add fund api binding

---
+++ wazirx-client/funds.go
@@ -0,0 +1,48 @@
+package wazirx_client
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"net/http"
+	helpers "wazirx-go/wazirx-client/Helpers"
+	api "wazirx-go/wazirx-client/endpoints"
+)
+
+type FundInfoService struct {
+	C *Client
+}
+
+type FundInfo struct {
+	Asset       string `json:"asset"`
+	Free        string `json:"free"`
+	Locked      string `json:"locked"`
+	ReservedFee string `json:"reservedFee"`
+}
+
+func (s *FundInfoService) FetchFundsInfo(ctx context.Context) *[]FundInfo {
+
+	r := &Request{
+		Method:     http.MethodGet,
+		Endpoint:   api.FUNDS,
+		SecType:    SecTypeSigned,
+		RecvWindow: 10000,
+		TimeStamp:  helpers.CurrentTimestamp(),
+	}
+
+	data, err := s.C.HandleAPI(ctx, r)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	result := new([]FundInfo)
+
+	if err := json.Unmarshal(data, &result); err != nil {
+		fmt.Println("Can not unmarshal JSON")
+	}
+
+	return result
+
+}
