89c8e5d4b82b52c2987fe4fcacb3e4b269fe689c
yuvan11 <yuva.ram11@gmail.com>
2022-03-19 15:58:00 +0530

    add test data to the func

--- wazirx-client/recentTrades_test.go
+++ wazirx-client/recentTrades_test.go
@@ -1,1 +1,56 @@
 package wazirx_client
+
+import (
+	"context"
+	"net/http"
+	"testing"
+
+	api "github.com/yuvan11/wazirx-go/wazirx-client/endpoints"
+)
+
+func TestRecentTradesService_DisplayRecentMarketTrade(t *testing.T) {
+	type args struct {
+		ctx context.Context
+	}
+	tests := []struct {
+		name    string
+		s       *RecentTradesService
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Recent Trade Service Test",
+			s: &RecentTradesService{
+				C: &Client{
+					ApiKey:     "",
+					Secretkey:  "",
+					BaseURL:    api.BASE_URL,
+					HTTPClient: http.Client{},
+					do: func(req *http.Request) (*http.Response, error) {
+						return &http.Response{}, nil
+					},
+				},
+				Symbol: "manausdt",
+				Limit:  10,
+			},
+			args: args{
+				ctx: context.TODO(),
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tt.s.DisplayRecentMarketTrade(tt.args.ctx)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("RecentTradesService.DisplayRecentMarketTrade() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if int32(len(*got)) != (tt.s.Limit) {
+				t.Errorf("RecentTradesService.DisplayRecentMarketTrade() = %v, want %v", len(*got), (tt.s.Limit))
+			}
+
+		})
+	}
+}
